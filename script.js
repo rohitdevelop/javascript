3e0278298523df69e212cc74c39bdfa500b6f606
Rohit singh <rohitdev124421@gmail.com>
2025-07-18 15:01:53 +0530

    js prep day 1

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const form = document.getElementById('contactForm');
+const button = document.getElementById('btn');
+const fullname = document.getElementById('name');
+const email = document.getElementById('email');
+const text = document.getElementById('massage');
 
 form.addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent page reload
+     e.preventDefault();
     form.reset(); // Clear all inputs
     alert('Form submitted successfully!');
 });
+
+button.addEventListener('click', function(e){
+    console.log(fullname.value,email.value,text.value);    
+})
