b3bb5e671f009c9976d013b98ed401b91ae0dc02
Rohit singh <rohitdev124421@gmail.com>
2025-07-29 13:35:59 +0530

    hosting

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-let name  = "rohit singh";
-let namecount  = [];
+//what is the Hosting
 
-for (let i = 0; i < name.length; i++) {
-  if (name[i]) {
-    namecount.push(name[i]).join();
-   } 
+//  hosit is like whne you declaration in a top of the vairavel like let var const and function also that is called hosting if the declaration in the top like
+
+// console.log(a); //undefive
+//  var a = "rhoti"
+
+//  console.log(b);//  Cannot access 'b' before initialization
+//  let b = "singh"
+
+// the correct way to declaration is after puting the value we can declare the
+// let a = "rohit singh"
+
+// console.log(a);
+// box1();
+// console.log(box2());
+
+// function box1() {
+//   let a = "coder";
+//   console.log(a);
+
+// }
+//   function box2() {
+//     let b = "coder2";
+//     return b;
+//   }
+
+
+let num = [1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 1; i <= num.length; i++) {
+ if (i % 2 === 0) {
+  console.log(i, "even");
+  
+ }  else{
+  console.log( i , "odd");
+  
+ }
 }
-
-console.log(namecount);
-// console.log(m
