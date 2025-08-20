53efb2ce8474dcace6425c3320235ae3e3d028b4
Rohit singh <rohitdev124421@gmail.com>
2025-08-20 16:44:40 +0530

    day 11

--- script.js
+++ script.js
@@ -1,67 +1,56 @@
+// 1
+// 12
+// 123
+// 1234
+// 12345
 for (let i = 1; i <= 5; i++) {
   let row = "";
-  for (let j = 1; j <= 5; j++) {
+  for (let j = 1; j <= i; j++) {
     row += j;
   }
   console.log(row);
 }
+// *
+// **
+// ***
+// ****
+// *****
+for (let i = 1; i <= 5; i++) {
+  let str = "* ";
+  console.log(str.repeat(i));
+}
+//  * * * * * 
+//   * * * * 
+//     * * * 
+//       * * 
+//         * 
+let a = 5;
+for (let i = a; i >= 1; i--) {
+    let str = "* ";
+    let space  = '  ';
+    console.log(space.repeat(a-i)+str.repeat(i));
+}
+ 
+//         * 
+//       * * 
+//     * * * 
+//   * * * * 
+// * * * * * 
+let b = 5;
+for (let i = 1; i <= b; i++) {
+    let str = "* ";
+    let space  = '  ';
+    console.log(space.repeat((b-i))+str.repeat(i));
+}
 
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-
-// const GymLoader = () => {
-//   return (
-//     <div className="w-full h-screen flex items-center justify-center bg-black">
-
-//       {/* Simple Dumbbell Animation */}
-//       <div className="flex flex-col items-center">
-
-//         {/* Animated Dumbbell */}
-//         <div className="flex items-center space-x-2 animate-pulse mb-8">
-//           {/* Left Weight */}
-//           <div className="w-5 h-14 bg-red-600 rounded-lg"></div>
-
-//           {/* Bar */}
-//           <div className="w-20 h-3 bg-gray-300 rounded-full"></div>
-
-//           {/* Right Weight */}
-//           <div className="w-5 h-14 bg-red-600 rounded-lg"></div>
-//         </div>
-
-//         {/* Loading Text */}
-//         <p className="text-white text-xl font-bold tracking-wider animate-pulse">
-//           LOADING...
-//         </p>
-
-//         {/* Simple Loading Dots */}
-//         <div className="flex space-x-2 mt-4">
-//           <div className="w-3 h-3 bg-red-600 rounded-full animate-bounce"></div>
-//           <div className="w-3 h-3 bg-red-600 rounded-full animate-bounce delay-200"></div>
-//           <div className="w-3 h-3 bg-red-600 rounded-full animate-bounce delay-400"></div>
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default GymLoader;
+let n = 5;
+for (let i = n; i >= 1; i--) {
+    let str = "* ";
+    console.log(str.repeat(i));
+}
