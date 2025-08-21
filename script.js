389425e208561c0ce018a9071612859358da5500
Rohit singh <rohitdev124421@gmail.com>
2025-08-21 15:59:37 +0530

    day 12

--- script.js
+++ script.js
@@ -1,56 +1,68 @@
-// 1
-// 12
-// 123
-// 1234
-// 12345
-for (let i = 1; i <= 5; i++) {
-  let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += j;
-  }
-  console.log(row);
-}
-// *
-// **
-// ***
-// ****
-// *****
-for (let i = 1; i <= 5; i++) {
-  let str = "* ";
-  console.log(str.repeat(i));
-}
-//  * * * * * 
-//   * * * * 
-//     * * * 
-//       * * 
-//         * 
-let a = 5;
-for (let i = a; i >= 1; i--) {
-    let str = "* ";
-    let space  = '  ';
-    console.log(space.repeat(a-i)+str.repeat(i));
-}
+// // 1
+// // 12
+// // 123
+// // 1234
+// // 12345
+// for (let i = 1; i <= 5; i++) {
+//   let row = ""
+//   for (let j = 1; j <= i; j++) {
+//     row += j;
+//     // console.log("inner");
+//   }
+//   console.log("outer",row);
+// }
+// // *
+// // **
+// // ***
+// // ****
+// // *****
+// for (let i = 1; i <= 5; i++) {
+//   let str = "* ";
+//   console.log(str.repeat(i));
+// }
+// //  * * * * * 
+// //   * * * * 
+// //     * * * 
+// //       * * 
+// //         * 
+// let a = 5;
+// for (let i = a; i >= 1; i--) {
+//     let str = "* ";
+//     let space  = '  ';
+//     console.log(space.repeat(a-i)+str.repeat(i));
+// }
  
-//         * 
-//       * * 
-//     * * * 
-//   * * * * 
-// * * * * * 
-let b = 5;
-for (let i = 1; i <= b; i++) {
-    let str = "* ";
-    let space  = '  ';
-    console.log(space.repeat((b-i))+str.repeat(i));
-}
+// //         * 
+// //       * * 
+// //     * * * 
+// //   * * * * 
+// // * * * * * 
+//  let b = 5;
+//  for (let i = 1; i <=  b; i++) {
+//  let str = "* "  
+//  let space = '  '
+//  console.log(space.repeat(b-i)+str.repeat(i));
+//  }
 
-// * * * * * 
-// * * * * 
-// * * * 
-// * * 
-// * 
+// // * * * * * 
+// // * * * * 
+// // * * * 
+// // * * 
+// // * 
+//  let c = 5;
+//  for (let i = c; i >= 1; i--) {
+//  let str = "* "
+//  console.log(str.repeat(i));
+//  } 
 
-let n = 5;
-for (let i = n; i >= 1; i--) {
-    let str = "* ";
-    console.log(str.repeat(i));
-}
+// //     * 
+// //    * * 
+// //   * * * 
+// //  * * * * 
+// // * * * * * 
+//  let d = 5
+// for (let i = 1; i <= d; i++) {
+//  let str = "* "
+//  let space = " "
+//  console.log(space.repeat(d-i)+str.repeat(i));
+// }
