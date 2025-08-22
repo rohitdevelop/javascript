83ab2736f43bf6d2c2aca517445b82dfbb3905c9
Rohit singh <rohitdev124421@gmail.com>
2025-08-22 16:50:15 +0530

    day 13

---
+++ GuessOutput.js
@@ -0,0 +1,36 @@
+console.log([] + {});//  {object  object}
+console.log({} + []);//  {object object}
+console.log([] + []);// {object}
+console.log({} + {});//  {object}
+ 
+let a = [1, 2, 3];
+let b = [1, 2, 3];
+console.log(a === b);//true
+console.log(a === b);//true
+console.log(a.toString() === b.toString());//true
+
+
+console.log(typeof NaN);//object
+console.log(NaN === NaN);//true
+console.log(Object.is(NaN, NaN));//
+
+
+let obj = {
+  a: 10,
+  getA: function () {
+    return this.a;
+  }
+};
+
+let fn = obj.getA;
+console.log(fn()); //undefine
+console.log(obj.getA()); //10
+
+
+async function test() {
+  return 1;
+}
+console.log(test());//error();
+
+
+test().then(res => console.log(res));//1
