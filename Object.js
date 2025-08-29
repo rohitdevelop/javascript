b19f824e5a6d07d59e8cb6cb5874a4d7e9c4d354
Rohit singh <rohitdev124421@gmail.com>
2025-08-29 16:50:40 +0530

    day 16

--- Object.js
+++ Object.js
@@ -1,10 +1,26 @@
-let object = {
+// let object = {
+//   name: "rohit singh",
+//   number: 454545,
+//   email: "rohit@gmail",
+//   person: {
+//     father: "triloki",
+//     mathoer: "savita",
+//   },
+// };
+// console.log(object.person);
+
+//1
+let stundent = {
     name: "rohit singh",
-    number:454545,
-    email:"rohit@gmail",
-   person: {
-father:"triloki",
-mathoer:"savita",
-    }
+    age:20,
+    cousrse:"bca"
 }
-console.log(object.person);
+//2
+let update = stundent.cousrse = "mca"
+// console.log(stundent);
+//3
+let add = stundent.grade=45
+console.log(stundent);
+let delet = delete stundent.age
+console.log(delet);
+
