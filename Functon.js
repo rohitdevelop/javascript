e7d611b4e3902e0c999fb9e8790153f15776fe82
Rohit singh <rohitdev124421@gmail.com>
2025-08-30 20:40:53 +0530

    day 17

---
+++ Functon.js
@@ -0,0 +1,34 @@
+// notes. Difference between split("") vs split(" ")
+// ✅ split("") → Split into characters ["r", "o", "h", "i", "t"]
+
+// ✅ split(" ") → Split into words (space-separated) ["rohit", "singh"]
+
+// function reverse(str) {
+//     let name = str.split("").reverse().join("");
+//     return name
+// }
+
+// console.log(reverse("rohit"));
+
+// number is even or odd
+
+// let user = prompt("write a number  1 to 100");
+// function guessnumber() {
+//   if (user % 2 === 0) {
+//     console.log(user, "this is a even number");
+//   } else {
+//     console.log(user, "this is a ood number");
+//   }
+// }
+// guessnumber()
+
+// fuctorial in function
+// function factorial(num) {
+//     let result = 1
+//   for (let i = 1; i <= num; i++) {
+//      result *= i; // same as result = result * i
+//    }
+//   return result
+// }
+// console.log(factorial(5));
+
--- script.js
+++ script.js
@@ -20,49 +20,67 @@
 //   let str = "* ";
 //   console.log(str.repeat(i));
 // }
-// //  * * * * * 
-// //   * * * * 
-// //     * * * 
-// //       * * 
-// //         * 
+// //  * * * * *
+// //   * * * *
+// //     * * *
+// //       * *
+// //         *
 // let a = 5;
 // for (let i = a; i >= 1; i--) {
 //     let str = "* ";
 //     let space  = '  ';
 //     console.log(space.repeat(a-i)+str.repeat(i));
 // }
- 
-// //         * 
-// //       * * 
-// //     * * * 
-// //   * * * * 
-// // * * * * * 
+
+// //         *
+// //       * *
+// //     * * *
+// //   * * * *
+// // * * * * *
 //  let b = 5;
 //  for (let i = 1; i <=  b; i++) {
-//  let str = "* "  
+//  let str = "* "
 //  let space = '  '
 //  console.log(space.repeat(b-i)+str.repeat(i));
 //  }
 
-// // * * * * * 
-// // * * * * 
-// // * * * 
-// // * * 
-// // * 
+// // * * * * *
+// // * * * *
+// // * * *
+// // * *
+// // *
 //  let c = 5;
 //  for (let i = c; i >= 1; i--) {
 //  let str = "* "
 //  console.log(str.repeat(i));
-//  } 
+//  }
 
-// //     * 
-// //    * * 
-// //   * * * 
-// //  * * * * 
-// // * * * * * 
+// //     *
+// //    * *
+// //   * * *
+// //  * * * *
+// // * * * * *
 //  let d = 5
 // for (let i = 1; i <= d; i++) {
 //  let str = "* "
 //  let space = " "
 //  console.log(space.repeat(d-i)+str.repeat(i));
+// }
+
+// let num = 6;
+// for (let i = 1; i < num; i++) {
+//   str = "* ";
+//   console.log(str.repeat(i));
+// }
+// for (let j = 4; j >= 1; j--) {
+//   str = "* ";
+//    console.log(str.repeat(j));
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i, "even");
+//   } else {
+//     console.log(i, "odd");
+//   }
 // }
