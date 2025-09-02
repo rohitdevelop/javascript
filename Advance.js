9b16ff401c9f761541e3f930d420894f2c21d8a4
Rohit singh <rohitdev124421@gmail.com>
2025-09-02 16:27:29 +0530

    day 20

---
+++ Advance.js
@@ -0,0 +1,25 @@
+// promis syntax
+
+// let promis = new Promise((res,rej)=>{
+//     let success = false
+
+//     if (success) {
+//         res("all is done")
+//     } else {
+//         rej("all is fail")
+//     }
+// })
+// console.log(promis);
+
+
+// let promise = new Promise((res,rej) =>{
+//     setTimeout(() => {
+//         res('kaise ho ajj app log')
+//     }, 2000);
+// })
+
+// promise.then((massage)=>{
+//     console.log('kais ho bolo maine bhi bola na :', massage);
+    
+// })
+
--- Object.js
+++ Object.js
@@ -10,17 +10,38 @@
 // console.log(object.person);
 
 //1
-let stundent = {
-    name: "rohit singh",
-    age:20,
-    cousrse:"bca"
-}
-//2
-let update = stundent.cousrse = "mca"
+// let stundent = {
+//     name: "rohit singh",
+//     age:20,
+//     cousrse:"bca"
+// }
+// //2
+// let update = stundent.cousrse = "mca"
+// // console.log(stundent);
+// //3
+// let add = stundent.grade=45
 // console.log(stundent);
-//3
-let add = stundent.grade=45
-console.log(stundent);
-let delet = delete stundent.age
-console.log(delet);
+// let delet = delete stundent.age
+// console.log(delet);
+
+// is aplicable to do vote or not
+
+// let username = prompt("write a name");
+// let age = prompt("write a age");
+// let email = prompt("write a email");
+
+// console.log(`name: ${username} 
+//              userage: ${age}
+//              email:${email}`);
+
+
+//              if (age <= 18 ) {
+//                 console.log("pagal sale nikal");
+                
+//             } else {
+//                 console.log("pagal sale aaja");
+                
+//              }
+
+
 
