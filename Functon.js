11c33a54dfcc2e92ee999ca07eeb83c12b3c2693
Rohit singh <rohitdev124421@gmail.com>
2025-11-12 11:34:00 +0530

    day 37

--- Functon.js
+++ Functon.js
@@ -32,7 +32,7 @@
 // }
 // console.log(factorial(5));
 
-// type of functions are 
+// type of functions are
 // Function Declaration, Function Expression, Arrow Function, Anonymous Function, Immediately Invoked Function Expression (IIFE), Recursive Function, Higher-Order Function, Callback Function, Constructor Function, Generator Function.
 
 // What is the difference between a function declaration and a function expression in JavaScript?
@@ -41,28 +41,28 @@
 // console.log(sum(2,3)); // works
 // function sum(a, b) {
 //   return a + b;
-// }//beacuse in that proper function and when you call the function anywher it will works 
+// }//beacuse in that proper function and when you call the function anywher it will works
 
 // console.log(mul(2,3)); // ❌ error
 // const mul = function(a, b) {
 //   return a * b;
-// };// and in tise case ther is diclare as a value in the const variable that way error come before insialisation 
+// };// and in tise case ther is diclare as a value in the const variable that way error come before insialisation
 
 // normal and arrowFunction
 // const obj = {
 //   value: 10,
 //   normalFunc: function() {
-//     console.log(this.value); 
+//     console.log(this.value);
 //   },//noraml function that can acces the global scop
 //   arrowFunc: () => {
-//     console.log(this.value); 
-//   }//and the arrow function not becuz or lexical scop 
+//     console.log(this.value);
+//   }//and the arrow function not becuz or lexical scop
 // };
 // obj.normalFunc(); // ?
 // obj.arrowFunc();  // ?
 // // why output are diffrent
 
-// max number in function 
+// max number in function
 // function largest(num) {
 //   let large = num[0];
 //   for (let i = 0; i < num.length; i++) {
@@ -74,7 +74,6 @@
 // }
 // console.log(largest([4, 55, 8, 75, 85, 65, 2]));
 
-
 // let num = [1,2,3,4,5,6,7,8,9,10]
 // function pexample() {
 //   let multi = num.map((n)=>n * 2)
@@ -82,7 +81,6 @@
 // }
 
 // console.log(pexample());
-
 
 // const obj = {
 //   a:1,
@@ -97,7 +95,6 @@
 // })
 // console.log(Object.entries(obj);
 
-
 // function sumnumder(rest) {
 //   let number = rest.reduce((a,v)=>{
 //     return a+v
@@ -107,18 +104,35 @@
 
 // console.log(sumnumder([45,5,5,8,10,60]));
 
+// function factorial() {
+//      let user = prompt("write a number to see factorial")
+//     let result  = 1
+//     for (let i  = 1; i  <= user; i++) {
+
+//          result *= i
+
+//     }
+//     return result
+//  }
+//  console.log(factorial())
+
+// src quetions easy level..........................................................
+
+// function ultimate(...num) {
+//  let sum = 0
+//   num.forEach( val => {
+//  sum += val    
+//   });
+//   return sum
+// }
+// console.log(ultimate(4, 5, 8, 9, 10, 5));
+
+// const rohit =  function (){
+//   let fun = "rohit"
+//   return fun
+// }
+//  console.log(rohit ()); //rohit 
+ 
 
 
-function factorial() {
-     let user = prompt("write a number to see factorial")
-    let result  = 1
-    for (let i  = 1; i  <= user; i++) {
-         
-         result *= i
-          
-    }
-    return result
- }
- console.log(factorial())
-
-
+console.log(typeof());
