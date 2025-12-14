345fad0330ac368e46334b854f99ffb029d102c1
Rohit singh <rohitdev124421@gmail.com>
2025-12-14 16:37:05 +0530

    day57

--- SCS.js
+++ SCS.js
@@ -67,7 +67,100 @@
 //     });
 //   });
 // });
+// let api = `bfc49e2b86ccf11a37a27d809c55f88c`;
+
+// async function getWeather(city) {
+//   try {
+//     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
+
+//     let raw = await fetch(url);
+//     let data = await raw.json();
+
+//     if (!raw.ok) {
+//         throw new Error("nhi....................");
+
+//     }
+
+//     console.log("Weather Data:", data);
+//   } catch (error) {
+//     console.log("Fetch error:", error);
+//   }
+// }
+
+// getWeather("Delhi");
+
+// let userEmails = [
+//   "rohit@45.com",
+//   "aman@45.com",
+//   "khushi@45.com",
+//   "ranveer@45.com",
+//   "rohitsingh@45female.com",
+// ];
+
+// function sendEmail(email) {
+//   return new Promise((resolve, reject) => {
+//     let randomTime = Math.floor(Math.random() * 5); // 0-4 seconds
+ 
+//     setTimeout(() => {
+//       let probability = Math.floor(Math.random() * 10);
+//       if (probability <= 5) {
+//          resolve(`Email to ${email} done`);
+//       } else {
+//          reject(`Email to ${email} not done`);
+//       }
+//     }, randomTime * 1000);
+//   });
+// }
+
+// async function sendEmails(userlist) {
+//    let allPromises = userlist.map((email) =>
+//     sendEmail(email).catch((err) => err)
+//   );
+
+//    let results = await Promise.all(allPromises);
+
+//   console.log("All results:", results);
+// }
+
+// sendEmails(userEmails);
+
+
+// debounsing and trottling
+
+// ...........................debounce......................................
+// function debounce(fun , dealy) {
+//     let timer;
+//     return function () {
+//         clearTimeout(timer)
+//         timer = setTimeout(fun, dealy)
+    
+//     }
+// }
+
+// document.querySelector("#select").addEventListener('input',debounce(function () {
+//     console.log("chala");
+    
+// },3000))
 
 
 
+// ...........................trottling......................................
 
+
+function trottling(fun , dealy) {
+    let last = 0;
+    return function () {
+    const now = Date.now();
+    if (now - last >= dealy) {
+        last = now;
+    fun();
+    }
+     
+    }
+}
+
+window.addEventListener('mousemove',trottling(function (e) {
+ console.log("hello");
+    
+},2000)
+)
