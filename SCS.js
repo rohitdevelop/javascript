6dc5b0257d3489e63b5f1b1cd5606c490051740f
Rohit Singh <rohitdev124421@gmail.com>
2026-03-03 17:30:07 +0530

    day 76

--- SCS.js
+++ SCS.js
@@ -165,4 +165,14 @@
 // },2000)
 // )
  
+ console.log("hello");
+ 
+
+ function revfun(src) {
+    let ulta = src.split("").reverse().join("")
+
+    return ulta
+ }
+
+ console.log(revfun("rohit"));
  
