97b67ac1427be488d480a0ace26cb1e166ede78a
Rohit Singh <rohitdev124421@gmail.com>
2026-06-23 21:04:39 +0530

    day 98

--- prep.js
+++ prep.js
@@ -1,1 +1,32 @@
 console.log("hello");
+
+// closers questions
+
+function getcounte(){
+    let num = 0;
+
+    let cunter = () => {
+       return num++
+    } 
+
+    return cunter
+}
+
+const counting = getcounte()
+
+console.log(counting()); // 0
+console.log(counting()); // 1
+
+ function sayHi() {
+    return "Hi";
+}
+
+console.log(sayHi);   // [Function: sayHi]
+console.log(sayHi()); // "Hi"
+
+
+// Closure JavaScript ka ek feature hai jisme ek function apne outer function ke variables ko yaad rakhta hai, even jab outer function execute hokar khatam ho chuka ho. Matlab inner function ke paas outer scope ke variables ka access rehta hai aur wo unki value ko use ya update kar sakta hai.
+
+
+
+
